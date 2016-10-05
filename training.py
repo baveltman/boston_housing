f0c16ed3112ffaba2546f1ecf3fb6060ca96d133
Boris Veltman <baveltman@gmail.com>
2016-10-04 23:22:58 -0700

    progress on project

---
+++ training.py
@@ -0,0 +1,37 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+import visuals as vs  # Supplementary code
+from sklearn.cross_validation import ShuffleSplit
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis=1)
+
+# Success
+print "Boston housing dataset has {} data points with {} variables each.".format(*data.shape)
+
+# Minimum price of the data
+minimum_price = np.amin(prices)
+
+# Maximum price of the data
+maximum_price = np.amax(prices)
+
+# Mean price of the data
+mean_price = np.mean(prices)
+
+# Median price of the data
+median_price = np.median(prices)
+
+# Standard deviation of prices of the data
+# not normalized by Bessel's correction
+std_price = np.std(prices)
+
+# Show the calculated statistics
+print "Statistics for Boston housing dataset:\n"
+print "Minimum price: ${:,.2f}".format(minimum_price)
+print "Maximum price: ${:,.2f}".format(maximum_price)
+print "Mean price: ${:,.2f}".format(mean_price)
+print "Median price ${:,.2f}".format(median_price)
+print "Standard deviation of prices: ${:,.2f}".format(std_price)
