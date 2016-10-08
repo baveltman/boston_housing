085e16de2b10940f3c61e8e88bbba0494bee0d26
Boris Veltman <baveltman@gmail.com>
2016-10-08 13:35:47 -0700

    project complete

--- training.py
+++ training.py
@@ -5,6 +5,9 @@ import visuals as vs  # Supplementary code
 from sklearn.cross_validation import ShuffleSplit
 from sklearn.metrics import r2_score
 from sklearn.cross_validation import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import make_scorer
+from sklearn.grid_search import GridSearchCV
 
 
 def performance_metric(y_true, y_predict):
@@ -16,6 +19,38 @@ def performance_metric(y_true, y_predict):
     # Return the score
     return score
 
+
+def fit_model(X, y):
+    """ Performs grid search over the 'max_depth' parameter for a
+        decision tree regressor trained on the input data [X, y]. """
+
+    # Create cross-validation sets from the training data
+    cv_sets = ShuffleSplit(X.shape[0], n_iter=10, test_size=0.20, random_state=0)
+
+    # Create a decision tree regressor object
+    regressor = DecisionTreeRegressor(random_state=0)
+
+    # Create a dictionary for the parameter 'max_depth' with a range from 1 to 10
+    max_depth_range = []
+
+    for x in range(1, 11):
+        max_depth_range.append(x)
+
+    params = {
+        'max_depth': max_depth_range
+    }
+
+    # Transform 'performance_metric' into a scoring function using 'make_scorer'
+    scoring_fnc = make_scorer(score_func=performance_metric)
+
+    # Create the grid search object
+    grid = GridSearchCV(estimator=regressor, param_grid=params, scoring=scoring_fnc, cv=cv_sets)
+
+    # Fit the grid search object to the data to compute the optimal model
+    grid = grid.fit(X, y)
+
+    # Return the optimal model after fitting the data
+    return grid.best_estimator_
 
 # Load the Boston housing dataset
 data = pd.read_csv('housing.csv')
@@ -55,7 +90,18 @@ X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=
 # Success
 print "Training and testing split was successful."
 
-# Produce learning curves for varying training set sizes and maximum depths
-vs.ModelLearning(features, prices)
 
-vs.ModelComplexity(X_train, y_train)
+# Fit the training data to the model using grid search
+reg = fit_model(X_train, y_train)
+
+# Produce the value for 'max_depth'
+print "Parameter 'max_depth' is {} for the optimal model.".format(reg.get_params()['max_depth'])
+
+# Produce a matrix for client data
+client_data = [[5, 17, 15], # Client 1
+               [4, 32, 22], # Client 2
+               [8, 3, 12]]  # Client 3
+
+# Show predictions
+for i, price in enumerate(reg.predict(client_data)):
+    print "Predicted selling price for Client {}'s home: ${:,.2f}".format(i+1, price)
